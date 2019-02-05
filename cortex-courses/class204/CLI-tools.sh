9d79f787e672b671d73354dff634f42612c660c0
npasricha-cs <npasricha-cs@cognitivescale.com>
2019-02-05 09:45:10 -0600

    cortex-courses unzipped

---
+++ cortex-courses/class204/CLI-tools.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# List Agents in your tenant
+cortex agents list
+
+
+# List Skills in your tenant
+cortex skills list
+
+
+#List Datasets in your tenant
+cortex datasets list
