7cdd6a3f35667745ac3a8668096fc6fb683a16d5
Coach Culbertson @ Cognitive Scale <45077825+cognitivecoach@users.noreply.github.com>
2019-03-14 11:18:29 -0500

    CleaningHouse
    Deleting old files to make way for new shiny lab files

--- class202/invoke_agent_cli.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# The command expects a file test_req.json in a test folder inside the skill. The file should include all the params including payload and properties.
-cortex actions invoke defautlt/echo --params-file test/test_req.json
